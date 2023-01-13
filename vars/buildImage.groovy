db8416270f993bca642b25ef3e157491424f4398
Ubuntu <ubuntu@ip-172-31-12-232.us-east-2.compute.internal>
2023-01-13 05:41:35 +0000

    f

---
+++ vars/buildImage.groovy
@@ -0,0 +1,13 @@
+def call() {
+    echo "building the docker image..."
+    withCredentials([string(credentialsId: 'dockerpasswd1', variable: 'dockerpasswd1')]) {
+         sh 'docker build -t mahesh2024/jenkins-sl:${BUILD_NUMBER} .'
+                   		 sh 'docker login -u mahesh2024 -p ${dockerpasswd1}'
+                   		 sh 'docker push mahesh2024/jekins-sl:${BUILD_NUMBER}'
+
+
+
+       
+    }
+} 
+
